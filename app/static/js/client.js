fe4bf74d6074b39c401f9345b450e8bbb78a9c63
Jules Larue <jules.larue@etu.univ-orleans.fr>
2016-04-02 11:09:19 +0200

    fonction js correctoin() pour la correction d'une question

--- app/static/js/client.js
+++ app/static/js/client.js
@@ -1,8 +1,14 @@
 
 // Quand le document HTML est prêt,
-// on fait une requêtes Ajax qui va récupérer
-// la question à afficher au client
+// on récupère une question à afficher
+// et on l'affiche
 $(document).ready(function() {
+  afficherNouvelleQuestion();
+})
+
+
+
+function afficherNouvelleQuestion() {
   $.ajax({
     url: "http://localhost:5000/instance/", // route api
     type: "GET", // requête GET
@@ -12,23 +18,35 @@ $(document).ready(function() {
       setInstanceQuestion(question, data["question_id"]);
       console.log("Temps restant : "+data["temps_restant"]);
       $("#countdown").countdown360({ // démarrage et affichage du compte à rebours
-      	radius      : 60, // rayon du timer
-      	seconds     : data["temps_restant"], // temps restant
-      	fontColor   : '#FFFFFF', // couleur du texte (ici, blanc)
-      	autostart   : true, // démarrage auto du timer
-      	onComplete  : function () { afficherStats(data["question_id"]) } // fonction à exécuter à la fin du compte à rebours
+      	radius               : 60, // rayon du timer
+      	seconds              : data["temps_restant"], // temps restant
+      	fontColor            : '#FFFFFF', // couleur du texte (ici, blanc)
+      	autostart            : false, // démarrage auto du timer
+        startOverAfterAdding : false,
+      	onComplete           : function () {
+          afficherStats(data["question_id"]); // affichage des stats de la question
+          if($("#rep1").is(":enabled"))
+            correction(data["question_id"]); // correction si pas de choix
+        } // fonction à exécuter à la fin du compte à rebours
       }).start();
-      $("#countdown").children().addClass("center-block"); // on centre le timer (enfant du div countdown)
-      // on modifie la largeur et la hauteur du timer
+      // Intialisation des boutons de choix de réponse
+      $("#rep1, #rep2").attr("disabled", false);
+      $("#rep1, #rep2").removeClass("btn-danger");
+      $("#rep1, #rep2").removeClass("btn-success");
+      $("#rep1, #rep2").addClass("btn-primary");
+
+      // Intialisation du timer
+      $("#countdown").children().addClass("center-block");
       $("#countdown").width(300);
       $("#countdown").height(300);
+
       $("#resultats").hide();
     },
     error: function(err){
       console.log("Erreur lors de la récupération des questions");
     }
   });
-})
+}
 
 
 function getQuestionById(question_id) {
@@ -68,6 +86,7 @@ function setInstanceQuestion(question, question_id) {
   // à la question dont l'id est e second paramètre est correcte ou non
   $("#rep1").attr("onclick", "envoyerReponse(1, "+question_id+")");
   $("#rep2").attr("onclick", "envoyerReponse(2, "+question_id+")");
+
 }
 
 
@@ -80,12 +99,8 @@ function envoyerReponse(numReponse, idQuestion) {
     datatype: "application/json", // type de donnée
     success:function(data) {
       if(data["success"] == true){
-        if(data["reponseCorrecte"] == true) { // le client a réponse correctement
-          indiquerBonneReponse(numReponse);
-
-        } else { // le client a mal réponse
-          indiquerMauvaiseReponse(numReponse);
-        }
+        console.log("appel correction("+idQuestion+")");
+        correction(idQuestion);
         // dans tous les cas, on rend les boutons inactifs
         $("#rep1, #rep2").attr("disabled", "disabled");
       }
@@ -101,32 +116,28 @@ function envoyerReponse(numReponse, idQuestion) {
 }
 
 /**
-  * Indiquer visuellement que la bonne réponse a été choisie
+  * Correction visuelle de la question (couleurs)
   */
-function indiquerBonneReponse(numReponse) {
-  $("#rep1, #rep2").removeClass("btn-primary"); // on enlève la classe qui affiche les boutons en bleu
-  $("#rep"+numReponse).addClass("btn-success"); // le bouton de la bonne réponse est un succès
-  var mauvaiseReponse = (numReponse==1) ? 2 : 1; // on récupère l'id de la mauvaise réponse
-  $("#rep"+mauvaiseReponse).addClass("btn-danger"); // le bouton de la mauvaise réponse se met en rouge
-
-  $("#msg-reponse").text("Bravo, c'est la bonne réponse !"); // on indique le texte de la bonne réponse
-  $("#msg-reponse").addClass("alert-success"); // le message est en vert (succès)
+function correction(id) {
+  $.ajax({
+    url: "http://localhost:5000/reponse/"+id, // route api
+    type: "GET", // requête GET
+    datatype: "application/json", // type de donnée
+    success:function(data) {
+      console.log("data = "+data)
+      console.log("correction question"+id+", bonne rép = "+data["reponseCorrecte"])
+      $("#rep1, #rep2").attr("disabled", true); // désactivation des boutons
+      $("#rep1, #rep2").removeClass("btn-primary"); // on enlève la classe qui affiche les boutons en bleu
+      $("#rep"+data["reponseCorrecte"]).addClass("btn-success"); // le bouton de la bonne réponse est un succès
+      var mauvaiseReponse = (data["reponseCorrecte"]==1) ? 2 : 1; // on récupère l'id de la mauvaise réponse
+      $("#rep"+mauvaiseReponse).addClass("btn-danger"); // le bouton de la mauvaise réponse se met en rouge
+    },
+    error: function(err){
+      console.log("Echec requête pour la bonne réponse.");
+    }
+  });
 }
 
-
-
-/**
-  * Indiquer visuellement que la bonne réponse a été choisie
-  */
-function indiquerMauvaiseReponse(numReponse) {
-  $("#rep1, #rep2").removeClass("btn-primary"); // on enlève la classe qui affiche les boutons en bleu
-  $("#rep"+numReponse).addClass("btn-danger"); // le bouton choisi est en rouge (vu qu'on a choisi la mauvaise réponse)
-  var bonneReponse = (numReponse==1) ? 2 : 1; // on récupère l'id de la bonne réponse
-  $("#rep"+bonneReponse).addClass("btn-success"); // le bouton de la bonne réponse se met en vert
-
-  $("#msg-reponse").text("Vous avez choisi la mauvaise réponse."); // on indique le texte de la bonne réponse
-  $("#msg-reponse").addClass("alert-danger"); // le message est en vert (succès)
-}
 
 
 function afficherStats(idQuestion) {
@@ -137,15 +148,13 @@ function afficherStats(idQuestion) {
     success:function(data) {
           console.log("nb bonnes : "+data["pourcentageBonnesReponses"]);
           console.log("nb mauvaises : "+data["pourcentageMauvaisesReponses"]);
-          if( !$("#resultats").is(":visible") ) {
           $("#countdown").fadeOut(callback = function() { // disparition chronomètre et apparition stats en fondu
-              // on fait l'affichage des stats que si elles ne le son pas déjà
-              $("#resultats").append($("<h3>").append("Résultats :"));
-              $("#resultats").append($("<p>").append("Bonnes réponses : "+data["pourcentageBonnesReponses"]+" %"));
-              $("#resultats").append($("<p>").append("Mauvaises réponses : "+data["pourcentageMauvaisesReponses"]+" %"));
-              $("#resultats").fadeIn();
-            });
-          }
+          // on fait l'affichage des stats que si elles ne le son pas déjà
+          $("#resultats").append($("<h3>").append("Résultats :"));
+          $("#resultats").append($("<p>").append("Bonnes réponses : "+data["pourcentageBonnesReponses"]+" %"));
+          $("#resultats").append($("<p>").append("Mauvaises réponses : "+data["pourcentageMauvaisesReponses"]+" %"));
+          $("#resultats").fadeIn();
+        });
     },
     error: function(err) {
       console.log("Erreur lors de la récupération des stats.");
